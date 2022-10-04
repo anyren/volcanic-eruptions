9c3c25c1061d66abd1cd682eddf2d1d75deea295
William Vann <billthelibrarian@gmail.com>
2022-10-03 20:23:51 -0500

    Working prototype of flask server with templates folder.

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import requests
+
+#############################################################
+# BEGIN FLASK ROUTING
+#############################################################
+
+
+app = Flask(__name__)
+
+
+
+#############################################################
+# Index endpoint
+#############################################################
+
+@app.route("/")
+def index():
+
+    url = "https://www.ngdc.noaa.gov/hazel/hazard-service/api/v1/volcanoes?maxYear=2022&minYear=2000"
+    response = requests.get(url)
+    json_object = response.json()
+    the_dict = dict(json_object)   
+    the_list = the_dict["items"]
+     
+    return render_template("index.html", the_list=the_list)
+            
+#############################################################
+# second endpoint
+#############################################################
+
+@app.route("/secondendpoint")
+def secondendpoint():
+
+    url = "https://www.ngdc.noaa.gov/hazel/hazard-service/api/v1/volcanoes?maxYear=2022&minYear=2000"
+    response = requests.get(url)
+    json_object = response.json()
+    the_dict = dict(json_object)  
+    the_list = the_dict["items"]
+
+    return render_template("secondendpoint.html", the_list=the_list)
+    
+#############################################################
+# third endpoint
+#############################################################
+
+
+@app.route("/thirdendpoint")
+def thirdendpoint():
+
+    url = "https://www.ngdc.noaa.gov/hazel/hazard-service/api/v1/volcanoes?maxYear=2022&minYear=2000"
+    response = requests.get(url)
+    json_object = response.json()
+    the_dict = dict(json_object)    
+    the_list = the_dict["items"]
+   
+    return render_template("thirdendpoint.html", the_list=the_list)
+
+#############################################################
+# fourth endpoint
+#############################################################
+
+@app.route("/fourthendpoint")
+def fourthendpoint():
+
+    url = "https://www.ngdc.noaa.gov/hazel/hazard-service/api/v1/volcanoes?maxYear=2022&minYear=2000"
+    response = requests.get(url)
+    json_object = response.json()
+    the_dict = dict(json_object)   
+    the_list = the_dict["items"]
+    
+    return render_template("fourthendpoint.html", the_list=the_list)
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#############################################################
+# END FLASK ROUTING
+#############################################################
+
